1766212e783de72efe3489e672c0a1f1ae536913
James Wrigley <jwrigley7@gmail.com>
2014-10-29 00:05:03 +1300

    Removed unnecessary #include.

--- src/InfoWidget.cpp
+++ src/InfoWidget.cpp
@@ -16,7 +16,6 @@
  *                                                                                *
  *********************************************************************************/
 
-#include <iostream>
 #include <QVariant>
 #include <QSqlQuery>
 #include <QSqlDatabase>
