70612e47034b8bde501b46a1a741a1fa16ae4d5e
James Wrigley <jwrigley7@gmail.com>
2014-10-29 22:07:36 +1300

    Set style programmatically. Todo: make this cross-platform.

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,13 @@
  *********************************************************************************/
 
 #include <QApplication>
+#include <QStyleFactory>
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
+  QApplication::setStyle(QStyleFactory::create("gtk"));
   MainWindow mainwindow;
   mainwindow.show();
 
